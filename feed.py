74e20f15e31fc17048edcfbe3097489793763656
Shivam Vats <123102767+shivam37483@users.noreply.github.com>
2024-08-30 18:06:40 +0000

    modified:   feed.py

--- feed.py
+++ feed.py
@@ -28,7 +28,7 @@ with open('feed.yaml', 'r') as file:
 
     for item in yaml_data['item']:
         item_element = xml_tree.SubElement(channel_element, 'item')
-
+ 
         xml_tree.SubElement(item_element, 'title').text = item['title']
         xml_tree.SubElement(item_element, 'itunes:author').text = yaml_data['author']
         xml_tree.SubElement(item_element, 'description').text = item['description']
