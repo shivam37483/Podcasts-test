bf56b045a4c50d211c68c2b300c81089ada585bc
Shivam Vats <123102767+shivam37483@users.noreply.github.com>
2024-09-04 20:26:31 +0530

    Update feed.py

--- feed.py
+++ feed.py
@@ -1,46 +1,57 @@
 import yaml
 import xml.etree.ElementTree as xml_tree
 
+# Define namespaces
+NSMAP = {
+    'itunes': 'http://www.itunes.com/dtds/podcast-1.0.dtd',
+    'content': 'http://purl.org/rss/1.0/modules/content/'
+}
+
+# Register namespaces
+for prefix, uri in NSMAP.items():
+    xml_tree.register_namespace(prefix, uri)
+
 with open('feed.yaml', 'r') as file:
     yaml_data = yaml.safe_load(file)
 
+    # Create RSS element with namespaces
     rss_element = xml_tree.Element('rss', {
-        'version':'2.0',
-        'xmlns:itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd',
-        'xmlns:content':'http://purl.org/rss/1.0/modules/content/'
+        'version': '2.0'
     })
-
+    
+    # Create channel element
     channel_element = xml_tree.SubElement(rss_element, 'channel')
 
     link_prefix = yaml_data['link']
 
+    # Populate the channel element with data from YAML
     xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
     xml_tree.SubElement(channel_element, 'format').text = yaml_data['format']
     xml_tree.SubElement(channel_element, 'subtitle').text = yaml_data['subtitle']
-    xml_tree.SubElement(channel_element, 'itune:author').text = yaml_data['author']
+    xml_tree.SubElement(channel_element, '{http://www.itunes.com/dtds/podcast-1.0.dtd}author').text = yaml_data['author']
     xml_tree.SubElement(channel_element, 'description').text = yaml_data['description']
-    xml_tree.SubElement(channel_element, 'itunes:image', {'href': link_prefix + yaml_data['image']})
+    xml_tree.SubElement(channel_element, '{http://www.itunes.com/dtds/podcast-1.0.dtd}image', {'href': link_prefix + yaml_data['image']})
 
     xml_tree.SubElement(channel_element, 'link').text = link_prefix
-    xml_tree.SubElement(channel_element, 'itunes:category', {'text':  yaml_data['category']})
+    xml_tree.SubElement(channel_element, '{http://www.itunes.com/dtds/podcast-1.0.dtd}category', {'text': yaml_data['category']})
 
-
-
+    # Create item elements for each item in the YAML
     for item in yaml_data['item']:
         item_element = xml_tree.SubElement(channel_element, 'item')
  
         xml_tree.SubElement(item_element, 'title').text = item['title']
-        xml_tree.SubElement(item_element, 'itunes:author').text = yaml_data['author']
+        xml_tree.SubElement(item_element, '{http://www.itunes.com/dtds/podcast-1.0.dtd}author').text = yaml_data['author']
         xml_tree.SubElement(item_element, 'description').text = item['description']
-        xml_tree.SubElement(item_element, 'itunes:duration').text = item['duration']
+        xml_tree.SubElement(item_element, '{http://www.itunes.com/dtds/podcast-1.0.dtd}duration').text = item['duration']
         xml_tree.SubElement(item_element, 'pubDate').text = item['published']
 
+        # Handle enclosure element with proper type casting for length
         enclosure = xml_tree.SubElement(item_element, 'enclosure', {
             'url': link_prefix + item['file'],
             'type': 'audio/mpeg',
-            'length': item['length']
+            'length': str(item['length'])  # Ensure this is a string
         })
 
+    # Output the RSS feed as an XML file
     output_tree = xml_tree.ElementTree(rss_element)
-    output_tree.write('podcast', xml_declaration=True, encoding='UTF-8')
-
+    output_tree.write('podcast.xml', xml_declaration=True, encoding='UTF-8')
